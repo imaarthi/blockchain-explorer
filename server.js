fdd4a82b9e051ef8f3aed571682b4e098c2e1a16
imaarthi <aarthi.cit@gmail.com>
2018-04-17 22:48:04 -0400

    Updated to read txns.csv and store it in chaindata.db

--- server.js
+++ server.js
@@ -4,9 +4,18 @@ var app = express();
 var engines = require('consolidate');
 var PORT = 8080;
 var bodyParser = require('body-parser');
-var stats = {};
+
+// Token from etherscan.io for getting data using APIs
 var etherScanToken = "JGSQ6DWADX27BD2FQ7ZT6NTP2TVDCI7CZS";
 
+// Database
+var db = require('any-db');
+var db_url = 'sqlite3://chaindata.db';
+var pool = db.createConnection(db_url);
+
+// CSV parser
+var csv =  require('csv');
+var csv_obj = csv();
 
 // copied from asignment page on website
 app.engine('html', engines.hogan); // tell Express to run .html files through Hogan
@@ -94,6 +103,120 @@ function getEtherLastPrice() {
 	console.log('result:' + json);
 }
 
+
+
+function createTables() {
+  pool.query('DROP TABLE transactions', function(err, res){
+    console.log("Unable to drop table transactions");
+  })
+
+  pool.query('CREATE TABLE IF NOT EXISTS transactions(TxHash TEXT PRIMARY KEY, BlockNo TEXT, UnixTimestamp TEXT, TxDate TEXT, From TEXT, To TEXT, Quantity TEXT)', function(error, data){
+      if(error) {
+        console.log("Unable to createTables");
+      }
+  })
+}
+
+function formattedTxns(txns) {
+  var formattedTxns = [];
+  for(var i=0; i< txns.length;i++) {
+    var tmp = {
+       "TxHash": txns[0],
+          "BlockNo": txns[1],
+          "UnixTimestamp": txns[2],
+          "txDate": txns[3],
+          "From": txns[4],
+          "To": txns[5],
+          "Quantity": txns[6]
+    }
+
+    formattedTxns.push(tmp);
+  }
+
+  return formattedTxns;
+}
+
+
+app.get('/transactions', getTxns);
+function getTxns(request, response) {
+  var sql = 'SELECT TxHash, BlockNo , txDate , From , To , Quantity FROM transactions';
+
+  pool.query(sql, function(error, result){
+      if(error) {
+        console.log("");
+      }else {
+        var txns = result.rows;
+        txns = formattedTxns(txns);
+        console.log("Rendering transactions.html");
+        response.render('transactions.html', { "txns": txns });
+      }
+  })
+
+}
+
+
+function insertDB(TxHash, BlockNo , UnixTimestamp , txDate , From , To , Quantity ){
+
+   pool.query('INSERT INTO transactions(TxHash, BlockNo , UnixTimestamp , TxDate , From , To , Quantity) VALUES($1, $2, $3, $4, $5, $6, $7)', [TxHash, BlockNo , UnixTimestamp , txDate , From , To , Quantity],  function(error, data){
+      if(error) {
+        console.log(error);
+      }
+  })
+
+}
+
+
+function readTxnsDataAndInsertInDB() {
+ // var data1 = [];
+
+  var txFile = "./txns.csv";
+  csv_obj.from.path(txFile).to.array(function (data) {
+    for (var i = 0; i < data.length; i++) {
+      //console.log(data[i][0])
+
+          insertDB(data[i][0],data[i][1],data[i][2],data[i][3],data[i][4],data[i][5],data[i][6]);
+        
+          // "TxHash": data[index][0],
+          // "BlockNo": data[index][1],
+          // "UnixTimestamp": data[index][2],
+          // "txDate": data[index][3],
+          // "From": data[index][4],
+          // "To": data[index][5],
+          // "Quantity": data[index][6]
+        
+
+       
+    }
+    
+   // console.log(data1);
+    //return data1;
+});
+
+
+
+}
+
+
+
+console.log("Creating the Database and tables");
+createTables();
+console.log("Created tables");
+
+console.log("Inserting transactions data into database");
+readTxnsDataAndInsertInDB();
+console.log("Inserted transactions data into database");
+
+
 // Server App listening 
 console.log("Server listening on Port: " +PORT);
 app.listen(PORT);
+
+
+
+
+
+
+
+
+
+
